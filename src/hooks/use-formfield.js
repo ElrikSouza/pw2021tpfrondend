67a3cde74236e99459eef07068438b2e68666a14
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-11-12 14:42:51 -0400

    useFormField hook refactored. useFormFieldWithoutValidation created.

--- src/hooks/use-formfield.js
+++ src/hooks/use-formfield.js
@@ -1,30 +1,41 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const useFormFieldWithoutValidation = (defaultValue = "") => {
   const [value, setValue] = useState(defaultValue);
+  const [wasTouched, setWasTouched] = useState(false);
 
   const onChange = ({ target }) => {
     setValue(target.value);
+    if (!wasTouched) {
+      setWasTouched(true);
+    }
   };
 
   return {
     value,
     setValue,
     onChange,
+    wasTouched,
   };
 };
 
 export const useFormField = (validator, defaultValue = "") => {
-  const [value, setValue] = useState(defaultValue);
   const [errors, setErrors] = useState([]);
   const [isValid, setIsValid] = useState(true);
+  const { value, wasTouched, ...rest } =
+    useFormFieldWithoutValidation(defaultValue);
 
-  const onChange = ({ target }) => {
-    setValue(target.value);
-    const errors = validator(target.value);
+  const triggerValidation = useCallback(() => {
+    const errors = validator(value);
     setErrors(errors);
     setIsValid(errors.length === 0);
-  };
+  }, [value, setErrors, setIsValid, validator]);
 
-  return { value, setValue, onChange, errors, isValid };
+  useEffect(() => {
+    if (wasTouched) {
+      triggerValidation();
+    }
+  }, [value, wasTouched, triggerValidation]);
+
+  return { value, wasTouched, errors, isValid, triggerValidation, ...rest };
 };
