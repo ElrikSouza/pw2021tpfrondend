5630f0d95682ac3ab63b1f036f0c428ee60fb663
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-11-14 17:29:36 -0400

    AboutPage; scaffolding created

---
+++ src/about/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const AboutPage = () => (
+  <div>
+    <h1>Sobre</h1>
+    <div>Sobre desc</div>
+    <div>Feito por Elrik Souza Silva, 2021.</div>
+  </div>
+);
