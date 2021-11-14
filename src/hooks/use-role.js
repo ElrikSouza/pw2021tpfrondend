8bde8f40b28a8b2895451fb0ab6fcbe6f4fad9dc
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-11-14 17:29:46 -0400

    created useRole hook

---
+++ src/hooks/use-role.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { store } from "../storage/storage";
+
+export const useRole = () => {
+  const [role, setRole] = useState("GUEST");
+
+  useEffect(() => {
+    const getRole = async () => {
+      const result = await store.getItem("role");
+      if (result) {
+        setRole(result);
+      } else {
+        setRole("GUEST");
+      }
+    };
+    getRole();
+  }, []);
+
+  return role;
+};
