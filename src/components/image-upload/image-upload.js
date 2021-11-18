54e0ea0a553428689ef4faf7ed63c0c2b2a0ae45
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-11-18 15:28:24 -0400

    refactored the image upload component

--- src/components/image-upload/image-upload.js
+++ src/components/image-upload/image-upload.js
@@ -1,10 +1,17 @@
 import React from "react";
+import "./image-upload.css";
 
 export const ImageUpload = ({ image, onChange }) => {
   const url = image instanceof Blob ? URL.createObjectURL(image) : "";
   return (
     <div>
-      {image !== "" && <img src={url} alt="Nenhum arquivo selecionado" />}
+      {image !== "" && (
+        <img
+          src={url}
+          alt="Nenhum arquivo selecionado"
+          className="image-upload_preview"
+        />
+      )}
       <input type="file" placeholder="Escolher arquivo" onChange={onChange} />
     </div>
   );
