fd2e2e3edae5b14f3e190cd6c03336fa3e205749
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2021-11-18 18:42:03 -0400

    updated index-products

--- src/products/index/index-products.js
+++ src/products/index/index-products.js
@@ -3,21 +3,24 @@ import { Pagination } from "../../components/pagination/pagination";
 import { SearchBar } from "../../components/search-bar/search-bar";
 import { ProductCard } from "./product-card";
 import { useProductsIndex } from "./use-products-index";
+import "./index-products.css";
 
 export const ProductsIndexPage = () => {
   const productsIndex = useProductsIndex();
 
   return (
-    <div>
+    <div className="index-grid">
       <SearchBar
         searchParam={productsIndex.searchParam}
         onChange={productsIndex.onChangeSearchParam}
         clear={productsIndex.clearSearch}
         submit={productsIndex.searchProductsByName}
       />
-      {productsIndex.products.map((product) => (
-        <ProductCard product={product} />
-      ))}
+      <div className="products-grid">
+        {productsIndex.products.map((product) => (
+          <ProductCard product={product} key={product.id} />
+        ))}
+      </div>
       <Pagination
         currentPage={productsIndex.currentPage}
         numOfPages={productsIndex.numberOfPages}
